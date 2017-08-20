e9e96fde0d514941cc1ddb97036fa4fc51ecbdf5
Noahb930 <noahb930@gmail.com>
2017-08-20 17:29:39 -0400

    Created and routed static_pages controller
    Added Home, About, Services, FAQ, Testimonials, and Contact pages.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def faq
+  end
+
+  def testimonials
+  end
+
+  def contact
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get services" do
+    get static_pages_services_url
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get static_pages_faq_url
+    assert_response :success
+  end
+
+  test "should get testimonials" do
+    get static_pages_testimonials_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
+end
